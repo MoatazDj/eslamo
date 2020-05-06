9ed538f51d2a6d8726705f1c3a52257b8dc8f294
unknown <nouhachorfi2@gmail.com>
2020-05-06 22:57:34 +0100

    test pomander

---
+++ database/user.js
@@ -0,0 +1,8 @@
+// let mysql =require('mysql')
+// let connection=mysql.createConnection({
+//     host: "localhost",
+//     user: "user",
+//     password: "password",
+//     database: "mydb"
+//   })
+// var table= "CREATE TABLE users (name varchar(10),email varchar(100) NOT NULL,password varchar(255) NOT NULL,) "
