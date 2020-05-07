9c7321b14885e0afccdab367f13278bad232ca59
raffa <raffa.rayen@gmail.com>
2020-05-07 13:28:22 +0100

    fix pomander styling

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-let http =require('http')
-let app=require('./App')
+let http = require('http');
+let app = require('./App');
 
-let server=http.createServer(app)
+let server = http.createServer(app);
 
-let port=process.env.port || 8000
+let port = process.env.port || 8000;
 
-server.listen(port,()=>{
-    console.log(`connected to server ${port}`)
-})
+server.listen(port, ()=>{
+  console.log(`connected to server ${port}`);
+});
