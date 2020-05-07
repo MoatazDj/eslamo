35a4bd41eca89f89bd36c084c181713e3b2cc0ca
Bouabda <abdaoui.wala@gmail.com>
2020-05-07 13:38:49 +0100

    check pommander

--- server/App.js
+++ server/App.js
@@ -1,15 +1,15 @@
 let express = require ('express');
-let bodyParser=require('body-parser')
-let cors =require('cors')
+let bodyParser = require('body-parser');
+let cors = require('cors');
 //let mysql=require('mysql')//verify
 //var User=require()// verify
 
 
-let app=express();
-app.use(cors())
-app.use(express.json())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
-app.use('/signup', require('./routers/signup'))
+let app = express();
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/signup', require('./routers/signup'));
 
-module.exports=app
+module.exports = app;
