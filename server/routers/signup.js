4c4b318b3a038f02f759d84186ad76ac452f0c44
MootezDj <djebalimootez1@gmail.com>
2020-05-09 01:10:16 +0300

    still testing

--- server/routers/signup.js
+++ server/routers/signup.js
@@ -22,50 +22,50 @@ router.post('/', (req, res, next) => {
 
     console.log('Received Signup POST request from client with ',userData);
 
-    const searchUserQuery = ' SELECT email FROM users WHERE email = ?'
-    const addUserQuery = `INSERT INTO users(first_name, last_name , email, gender , phone_number) VALUES(?,?,?,?,?)`;
-    const addPassword = `INSERT INTO passwords(user_password,salt,user_id) VALUES(?,?,?)`;
-    const userInfo = [userData.first_name, userData.last_name, userData.email, userData.gender, userData.phone_number]
-    const salt = bcrypt.genSaltSync(28, "a");
+    // const searchUserQuery = ' SELECT email FROM users WHERE email = ?'
+    // const addUserQuery = `INSERT INTO users(first_name, last_name , email, gender , phone_number) VALUES(?,?,?,?,?)`;
+    // const addPassword = `INSERT INTO passwords(user_password,salt,user_id) VALUES(?,?,?)`;
+    // const userInfo = [userData.first_name, userData.last_name, userData.email, userData.gender, userData.phone_number]
+    // const salt = bcrypt.genSaltSync(28, "a");
 
-    connection.query(searchUserQuery, [userData.email], (err, result) => {
-        console.log(3)
-        console.log("result  " + result)
-        if (err) console.log("err2 " + err)
-        else {
-            console.log(4)
-            console.log("result  " + result)
-            if (result.length > 0) {
-                res.json({
-                    message: 'user exist'
-                })
+    // connection.query(searchUserQuery, [userData.email], (err, result) => {
+    //     console.log(3)
+    //     console.log("result  " + result)
+    //     if (err) console.log("err2 " + err)
+    //     else {
+    //         console.log(4)
+    //         console.log("result  " + result)
+    //         if (result.length > 0) {
+    //             res.json({
+    //                 message: 'user exist'
+    //             })
 
-            } else {
-                connection.query(addUserQuery, userInfo, (err, resl) => {
-                    console.log(5)
-                    console.log("result555  " + resl)
-                    if (err) console.log('err5 ' + err)
-                    // resl => {
-                    console.log('resl ' + salt)
-                    // crypto.pbkdf2(userData.password, salt,4, 28, 'sha512' ,(err,hash) => {
-                    bcrypt.hash(userData.password, salt, (err, hash) => {
-                        if (err) console.log('errhash' + hash)
-                        console.log('ok ' + hash)
-                        connection.query(addPassword, [hash, salt, resl.insertId], (error, resPass) => {
-                            if (error) console.log('errPAssword ' + error)
-                            console.log(resPass)
-                            // resPass => {
-                            console.log('add sucess')
-                            res.json({
-                                message: 'user created successfully'
-                            })
-                            // }
-                        })
-                    })
-                    // }
-                })
-            }
-        }
-    })
+    //         } else {
+    //             connection.query(addUserQuery, userInfo, (err, resl) => {
+    //                 console.log(5)
+    //                 console.log("result555  " + resl)
+    //                 if (err) console.log('err5 ' + err)
+    //                 // resl => {
+    //                 console.log('resl ' + salt)
+    //                 // crypto.pbkdf2(userData.password, salt,4, 28, 'sha512' ,(err,hash) => {
+    //                 bcrypt.hash(userData.password, salt, (err, hash) => {
+    //                     if (err) console.log('errhash' + hash)
+    //                     console.log('ok ' + hash)
+    //                     connection.query(addPassword, [hash, salt, resl.insertId], (error, resPass) => {
+    //                         if (error) console.log('errPAssword ' + error)
+    //                         console.log(resPass)
+    //                         // resPass => {
+    //                         console.log('add sucess')
+    //                         res.json({
+    //                             message: 'user created successfully'
+    //                         })
+    //                         // }
+    //                     })
+    //                 })
+    //                 // }
+    //             })
+    //         }
+    //     }
+    // })
 })
 module.exports = router
