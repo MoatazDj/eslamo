eb05c702b46877c614891e61beba83668a3b09c9
unknown <nouhachorfi2@gmail.com>
2020-05-09 22:19:08 +0100

    verses query updated

--- server/routers/verses.js
+++ server/routers/verses.js
@@ -4,16 +4,19 @@ const connection = require('../../database/index');
 
 router.get('/', (req, res, next) => {
     //const state = req.query.state;
-    const state = 'angry'
-    console.log('Verses GET request received with state :', state);
+    const state = 'scared'
+    //console.log('Verses GET request received with state :', state);
 
-    connection.query('select verse_number, surah_number from verses v inner join ' +
+    connection.query('select * from verses v inner join ' +
         'states_verses sv  on sv.verse_id = v.verse_id inner join ' +
         'emotional_states es on sv.emotional_state_id = es.emotional_state_id where emotional_state =\'' + state + '\';', (err, results) => {
             if (err) throw err;
-            console.log(results[0]);
+            // console.log(results[0]);
             // console.log(results[0].verse_number);
             res.send(results);
         });
 });
+router.post('/', (req, res) => {
+    console.log(req.body)
+})
 module.exports = router;
