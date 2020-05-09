068370db080904ed0a6fb50d909161afe40b585c
Bouabda <abdaoui.wala@gmail.com>
2020-05-09 16:29:32 +0100

    get verse data from db

--- server/routers/verses.js
+++ server/routers/verses.js
@@ -7,12 +7,12 @@ router.get('/', (req, res, next) => {
 
     console.log('Verses GET request received with state :', state);
 
-    connection.query('select verse_number, surah_number from verses v inner join'+ 
-    'states_verses sv  on sv.verse_id = v.verse_id inner join'+ 
-    'emotional_states es on sv.emotional_state_id = es.emotional_state_id where emotional_state =\'state\';', (err, results) => {
+    connection.query('select verse_number, surah_number from verses v inner join '+ 
+    'states_verses sv  on sv.verse_id = v.verse_id inner join '+ 
+    'emotional_states es on sv.emotional_state_id = es.emotional_state_id where emotional_state =\''+ state +'\';', (err, results) => {
      if(err) throw err;
-     console.log(results);
+     console.log(results[0].verse_number);
      res.send(results);
     });
 });
-module.exports = router
+module.exports = router;
